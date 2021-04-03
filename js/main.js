9b15cf7ee1e7da3752fc54af053f82c0a6879971
Juanes-GH <jjgohe3@gmail.com>
2021-04-03 15:54:44 +0200

    new pudate

--- js/main.js
+++ js/main.js
@@ -1,21 +1,12 @@
-function getRandomInt(min, max) {
-    return Math.floor(Math.random() * (max - min)) + min;
-}
- console.log(getRandomInt(1, 151));
 
-const bringData = (id) =>{
-        fetch(`https://pokeapi.co/api/v2/${id}`)
-        .then= (res => res.json())
-        .then(data => {
-            bringData.innerHTML = `
-                    <img src = "${data.results['0'].picture.large}"`
-        
-    })
-    
+const $pokemonimg = document.querySelector('#pokemonimg')
+function renderPokemon(image){
+  $pokemonimg.setAttribute('scr', image)
 }
 
-const paintCart = (pokemon) => {
-    console.log(pokemon)
-    const cPokemon1 = getSelection
-  
-}
+
+fetch('https://pokeapi.co/api/v2/pokemon/25/')
+  .then(response => response.json())
+  .then(pokemon => {
+    renderPokemon(pokemon.sprites.front_default)
+  })
