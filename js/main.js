a9f70d63fea89723ef4c31238d9df9d3370c642b
Juanes-GH <jjgohe3@gmail.com>
2021-04-04 19:35:48 +0200

    new update

--- js/main.js
+++ js/main.js
@@ -1,12 +1,39 @@
+let lista = document.getElementById("listaPokemon")
 
-const $pokemonimg = document.querySelector('#pokemonimg')
-function renderPokemon(image){
-  $pokemonimg.setAttribute('scr', image)
-}
-
-
-fetch('https://pokeapi.co/api/v2/pokemon/25/')
-  .then(response => response.json())
-  .then(pokemon => {
-    renderPokemon(pokemon.sprites.front_default)
+function consultarPokemon(id, num){
+fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
+  .then(function (response){
+    response.json()
+    .then(function(pokemon){
+      crearPokemon(pokemon, num)
+    })
   })
+  
+  }
+
+
+  function consultarPokemones() {
+    let primerId = Math.round(Math.random()* 150)
+    let segundoId =  Math.round(Math.random() * 150)
+    let thirdId =  Math.round(Math.random() * 150)
+    let fourthId =  Math.round(Math.random() * 150)
+    
+    consultarPokemon(primerId, 1)
+    consultarPokemon(segundoId, 2)
+    consultarPokemon(thirdId, 3)
+    consultarPokemon(fourthId, 4)
+    
+ 
+  }
+ 
+ function crearPokemon(pokemon, num){
+   let item = lista.querySelector(`#pokemon-${num}`)
+   
+   let imagen = item.getElementsByTagName("img")[0]
+   imagen.setAttribute("src", pokemon.sprites.front_default)
+   
+   let nombre = item.getElementsByTagName("p")[0]
+   nombre.textContent = pokemon.name
+
+ }
+ consultarPokemones()
